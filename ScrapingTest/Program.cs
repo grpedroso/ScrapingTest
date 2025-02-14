c8d39f83bfbb94841e6f947787c8e778a98bc506
Gabriel Pedroso <gabrielpedrosor@gmail.com>
2025-02-13 21:02:18 -0300

    Calling a website

--- ScrapingTest/Program.cs
+++ ScrapingTest/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+static async Task<string> CallUrl(string url)
+{
+    HttpClient client = new HttpClient();
+    var response = await client.GetStringAsync(url);
+    return response;
+}
+
+
+    string url = "https://es.tradingview.com/symbols/BTCUSD/";
+    var response = CallUrl(url).Result;
+    Console.WriteLine(response);
